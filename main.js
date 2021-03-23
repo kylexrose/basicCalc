d7662cfc27a516e2d65f833f856b9d118e36a061
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-23 17:39:17 -0400

    calculator functionality complete

--- main.js
+++ main.js
@@ -30,5 +30,45 @@ function darkModeOn(){
     document.querySelector("#screen").style.backgroundColor = "black";
     document.querySelector("#screen p").style.color = "white";
 }
+let firstNum = "";
+let operation = "";
+let secondNum = "";
+console.log(firstNum)
+const screen = document.querySelector("#screen p");
+const display = document.querySelector("#screen p");
+const buttonDiv = document.querySelector("#buttonDiv");
+buttonDiv.addEventListener('click', (e)=> addKeyToString(e.target));
 
-change
+function addKeyToString(key){
+    console.log(firstNum, operation, secondNum, key.id)
+    if(!operation && (!isNaN(key.innerHTML) || (key.innerHTML === "." && !(firstNum.includes("."))))){
+        firstNum += key.innerHTML;
+        console.log(firstNum)
+    }else if(key.id === "clear"){
+        firstNum = "";
+        operation = "";
+        secondNum = "";
+    }else if(firstNum && !secondNum &&
+    (key.id === "div" || key.id === "mult" || 
+    key.id === "min" || key.id === "plus")){
+        operation = key.innerHTML;
+    }else if(firstNum && operation && (!isNaN(key.innerHTML) || (key.innerHTML === "." && !(secondNum.includes("."))))){
+        secondNum += key.innerHTML;
+    }else if(firstNum && operation && secondNum && 
+        (key.id === "div" || key.id === "mult" || 
+        key.id === "min" || key.id === "plus" || 
+        key.id === "eql")){
+            firstNum = eval(screen.innerHTML);
+            operation = "";
+            secondNum = "";
+            if(key.id === "div" || key.id === "mult" || 
+            key.id === "min" || key.id === "plus" ){
+                operation = key.innerHTML;
+            }
+        }
+    updateScreen();
+}
+
+function updateScreen(){
+    screen.innerHTML = `${firstNum} ${operation} ${secondNum}`;
+}
